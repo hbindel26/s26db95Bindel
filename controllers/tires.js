c4a624076ff4d47e34ece723aa9cbf3b2b055a95
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-04-08 18:45:57 -0500

    part 7

--- controllers/tires.js
+++ controllers/tires.js
@@ -18,9 +18,23 @@ exports.tires_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Tires detail: ' + req.params.id);
 };
 
-// Handle Tire create on POST
-exports.tires_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Tires create POST');
+// Handle Tire create on POST.
+exports.tires_create_post = async function(req, res) {
+    console.log(req.body);
+    let document = new Tires();
+    // We are looking for a body, since POST does not have query parameters.
+    // We require that it be a JSON object
+    document.tire_type = req.body.tire_type;
+    document.size = req.body.size;
+    document.cost = req.body.cost;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch(err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
 // Handle Tire delete on DELETE

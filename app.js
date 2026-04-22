9019c00a5f0f12e2c1ad82f329263e5ded454227
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-04-22 18:58:11 -0500

    edit3

--- app.js
+++ app.js
@@ -80,7 +80,6 @@ app.use('/pick', pickRouter);
 app.use('/resource', resourceRouter);
 // passport config
 // Use the existing connection
-var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());

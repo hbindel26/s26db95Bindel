3ba5210c3c985b9d5bccc21ed071df3b71fda148
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-04-22 19:55:53 -0500

    part 5

--- routes/tires.js
+++ routes/tires.js
@@ -2,13 +2,16 @@ var express = require('express');
 const tires_controllers = require('../controllers/tires');
 var router = express.Router();
 
-/* GET tires page. */
-router.get('/', tires_controllers.tires_view_all_Page);
-/* GET detail tire page */
-router.get('/detail', tires_controllers.tires_view_one_Page);
-/* GET update tire page */
-router.get('/update', tires_controllers.tires_update_Page);
-/* GET delete tire page */
-router.get('/delete', tires_controllers.tires_delete_Page);
+// routes/tires.js
+const secured = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+// Apply to protected routes
+router.get('/update', secured, tires_controllers.tires_update_Page);
+router.get('/delete', secured, tires_controllers.tires_delete_Page);
 
 module.exports = router;
